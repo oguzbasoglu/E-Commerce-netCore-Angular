7f2f2bc165b5ee7ffaffa6cf2f9bdd63893e47aa
Oğuz Başoğlu <oguzbasoglu12@outlook.com>
2021-04-26 12:20:40 +0300

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+
+        [HttpGet("GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("GetProduct/{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.Where(c => c.Id == id).FirstOrDefaultAsync();
+
+            return product;
+        }
+    }
+}
